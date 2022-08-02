c347f1120a3501140c5817f56e6e5896a979d14a
Yuuta Segawa <yuuta@192.168.100.3>
2022-08-02 14:32:03 +0300

    fix: missed src file

---
+++ src/js/_components.js
@@ -0,0 +1,13 @@
+import './components/smooth-scroll.js'
+import './components/hero-int.js'
+import './components/header.js'
+import './components/tool-tipe.js'
+import './components/modals.js'
+import './components/slider.js'
+import './components/price-int.js'
+import './components/validation.js'
+import './components/form-int.js'
+import './components/on-entry.js'
+import './components/borger.js'
+
+
